4be6627d278bcde233cb42e96aa6b57e7d26fd44
kanji.kurita <coderkkurita@gmail.com>
2023-04-21 19:16:14 +0900

    commentout

--- assets/javascripts/video_embed.js
+++ assets/javascripts/video_embed.js
@@ -1,39 +1,39 @@
-// plugins/redmine_video_embed/assets/javascripts/video_embed.js
-$(document).ready(function () {
-    var commentArea = $('#notes');
-    if (!commentArea.length) {
-        return;
-    }
+// // plugins/redmine_video_embed/assets/javascripts/video_embed.js
+// $(document).ready(function () {
+//     var commentArea = $('#notes');
+//     if (!commentArea.length) {
+//         return;
+//     }
 
-    commentArea.on('dragover', function (e) {
-        e.preventDefault();
-        e.stopPropagation();
-    });
+//     commentArea.on('dragover', function (e) {
+//         e.preventDefault();
+//         e.stopPropagation();
+//     });
 
-    commentArea.on('drop', function (e) {
-        e.preventDefault();
-        e.stopPropagation();
+//     commentArea.on('drop', function (e) {
+//         e.preventDefault();
+//         e.stopPropagation();
 
-        var files = e.originalEvent.dataTransfer.files;
+//         var files = e.originalEvent.dataTransfer.files;
 
-        if (!files || files.length === 0) {
-            return;
-        }
+//         if (!files || files.length === 0) {
+//             return;
+//         }
 
-        for (var i = 0; i < files.length; i++) {
-            var file = files[i];
-            var fileType = file.type;
+//         for (var i = 0; i < files.length; i++) {
+//             var file = files[i];
+//             var fileType = file.type;
 
-            if (fileType.startsWith('video/')) {
-                var reader = new FileReader();
-                reader.onload = (function (file) {
-                    return function (e) {
-                        var embedCode = '!video(' + e.target.result + ')!';
-                        commentArea.val(commentArea.val() + '\n' + embedCode);
-                    };
-                })(file);
-                reader.readAsDataURL(file);
-            }
-        }
-    });
-});
+//             if (fileType.startsWith('video/')) {
+//                 var reader = new FileReader();
+//                 reader.onload = (function (file) {
+//                     return function (e) {
+//                         var embedCode = '!video(' + e.target.result + ')!';
+//                         commentArea.val(commentArea.val() + '\n' + embedCode);
+//                     };
+//                 })(file);
+//                 reader.readAsDataURL(file);
+//             }
+//         }
+//     });
+// });
